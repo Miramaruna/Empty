6c848bb312078377a1ab3a02b375764389c672b8
Miramaruna <miruna2011kk@gmail.com>
2025-01-06 19:20:43 +0600

    Exit

--- empty.py
+++ empty.py
@@ -2,7 +2,10 @@ import time
 
 
 attempt = 0
-while True:
-    attempt += 1
-    print(F"{attempt}: empty")
-    time.sleep(0.1)
+try:
+    while True:
+        attempt += 1
+        print(F"{attempt}: empty")
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    print("\nExiting...")
